dfd17953675d7bff166eb8a9b0dc046a3934a93c
Ishwar Singh <ishwar.singh@neosoftmail.com>
2025-01-17 17:56:05 +0530

    Update footer github link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 const Footer = () => {
     const year = new Date().getFullYear();
     const content = (
         <div className="flex justify-between p-5">
-                <p>Next Shopping &copy; {year}</p>
-                <p>Made with ❤️ by dev-ishwar</p>
-            </div>
+            <p>Next Shopping &copy; {year}</p>
+            <p>
+                Made with ❤️ by &nbsp;
+                <Link href={'https://github.com/dev-ishwar'} target="_blank">dev-ishwar</Link>
+            </p>
+        </div>
     )
 
     return (
